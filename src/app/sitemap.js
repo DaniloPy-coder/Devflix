5f1f5ec91d470936ec7769d04b55a9b5966d6c55
Danilo <danilodevsp@gmail.com>
2025-08-17 23:36:39 -0300

    correcao sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,12 +1,15 @@
-const movies = await fetch('https://api.devflix.com/movies').then(res => res.json());
-const movieUrls = movies.map(movie => ({
-    url: `${baseUrl}/movie/${movie.id}`,
-    lastModified: new Date(movie.updatedAt),
-    changeFrequency: 'weekly',
-    priority: 0.7
-}));
+export default async function sitemap() {
+    const baseUrl = 'https://devflix-drab.vercel.app';
+    const movies = await fetch('https://api.devflix.com/movies').then(res => res.json());
+    const movieUrls = movies.map(movie => ({
+        url: `${baseUrl}/movie/${movie.id}`,
+        lastModified: new Date(movie.updatedAt),
+        changeFrequency: 'weekly',
+        priority: 0.7
+    }));
 
-return [
-    { url: `${baseUrl}/`, lastModified: new Date(), changeFrequency: 'weekly', priority: 1.0 },
-    ...movieUrls
-];
+    return [
+        { url: `${baseUrl}/`, lastModified: new Date(), changeFrequency: 'weekly', priority: 1.0 },
+        ...movieUrls
+    ];
+}
