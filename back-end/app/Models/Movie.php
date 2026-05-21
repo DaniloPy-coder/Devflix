f51020fff4bf5be8fe52e9372acf6615be45befd
Danilo <danilodevsp@gmail.com>
2026-05-21 11:09:28 -0300

    feat: cria esquema do banco de dados para filmes, generos e usuarios

---
+++ back-end/app/Models/Movie.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany; 
+
+class Movie extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'overview',
+        'poster_path',
+        'release_date',
+    ]; 
+
+    public function genres(): BelongsToMany
+    {
+        return $this->belongsToMany(Genre::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
