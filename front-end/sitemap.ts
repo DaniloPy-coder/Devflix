23e2ae70ceb30eca315a18d69a3f2f19c28b5b06
Danilo <danilodevsp@gmail.com>
2026-05-22 16:05:40 -0300

    chore: atualiza rotas do sitemap para incluir filmes locais

--- front-end/sitemap.ts
+++ front-end/sitemap.ts
@@ -3,13 +3,27 @@ import { MetadataRoute } from "next";
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
 
-  const routes = [
+  const localMovies = await fetch("http://127.0.0.1:8000/api/movies")
+    .then((res) => res.json())
+    .catch(() => []);
+
+  const routes: MetadataRoute.Sitemap = [
     {
       url: baseUrl,
       lastModified: new Date(),
-      changeFrequency: "daily" as const,
+      changeFrequency: "daily",
+      priority: 1,
     },
   ];
+
+  localMovies.forEach((movie: { id: number | string }) => {
+    routes.push({
+      url: `${baseUrl}/movie/local/${movie.id}`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    });
+  });
 
   return routes;
 }
