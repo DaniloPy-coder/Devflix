6d3c74bb689cf78856efb86bc27c22a0dd104e36
Danilo <danilodevsp@gmail.com>
2026-05-24 14:39:30 -0300

    chore: mudei a url do frontend

--- back-end/config/cors.php
+++ back-end/config/cors.php
@@ -6,7 +6,7 @@ return [
     
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://devflix-olive.vercel.app'], 
+    'allowed_origins' => ['https://devflix-sable.vercel.app'], 
 
     'allowed_origins_patterns' => [],
 
