13ee2d52637f21b01c0edd35ebf53ef4e937c2c5
Danilo <danilodevsp@gmail.com>
2026-05-23 22:54:06 -0300

    fix: corrige bloqueio CORS e ajusta rotas de filmes locais

--- back-end/config/cors.php
+++ back-end/config/cors.php
@@ -5,7 +5,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'], 
+    'allowed_origins' => ['https://devflix-olive.vercel.app'], 
 
     'allowed_origins_patterns' => [],
 
