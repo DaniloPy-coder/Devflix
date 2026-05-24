e90451d26bcc00bc9de2390db4de9628dc9789b6
Danilo <danilodevsp@gmail.com>
2026-05-24 16:17:34 -0300

    fix: corrigindo o metodo de validacao

--- back-end/router.php
+++ back-end/router.php
@@ -1,15 +1,15 @@
 <?php
 
-header("Access-Control-Allow-Origin: https://devflix-sable.vercel.app");
+header("Access-Control-Allow-Origin: *"); 
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, X-XSRF-TOKEN");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, X-XSRF-TOKEN, bypass-tunnel-reminder");
 header("Access-Control-Allow-Credentials: true");
 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
     exit;
 }
 
-// Se não for um arquivo existente, direciona para o index.php
 if (file_exists(__DIR__ . '/public' . $_SERVER['REQUEST_URI'])) {
     return false;
 }
