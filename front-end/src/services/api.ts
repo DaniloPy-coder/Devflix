6918318bf17a3121534d778cfe766cb8657af980
Danilo <danilodevsp@gmail.com>
2026-05-24 17:42:20 -0300

    fix(api): altera baseURL para / para ativar proxy reverso

--- front-end/src/services/api.ts
+++ front-end/src/services/api.ts
@@ -1,12 +1,11 @@
 import axios from "axios";
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL;
-
-export const api = axios.create({
-  baseURL: baseURL,
-  withCredentials: false,
+const api = axios.create({
+  baseURL: "/",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
   },
 });
+
+export { api };
