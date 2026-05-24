b149a6dc26f80c1eedb08341cb9966e215e4fbe2
Danilo <danilodevsp@gmail.com>
2026-05-24 15:55:51 -0300

    feat(frontend): configurar api.ts para integração com o backend

--- front-end/src/services/api.ts
+++ front-end/src/services/api.ts
@@ -4,7 +4,7 @@ const baseURL = process.env.NEXT_PUBLIC_API_URL;
 
 export const api = axios.create({
   baseURL: baseURL,
-  withCredentials: true,
+  withCredentials: false,
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
